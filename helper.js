1c0fd607d44a1118edcbf2be7ab6daa3806f711a
Harzz <basuharish97@gmail.com>
2019-08-20 22:34:06 +0530

    functions to generate and validate jwt

--- helper.js
+++ helper.js
@@ -1,4 +1,6 @@
 var bcrypt = require('bcrypt')
+var jwt = require('jsonwebtoken')
+var jwtPassphrase = 'northkorea'
 var rounds = 8
 module.exports.generateHash = function (password)  {
     
@@ -6,4 +8,22 @@ module.exports.generateHash = function (password)  {
     var hash = bcrypt.hashSync(password, salt);
     console.log(hash)    
     return hash
+}
+
+module.exports.generateJWT = function(user,id){
+    var payload = {
+        exp:Math.floor(Date.now() / 1000) + (18000),
+        data: {
+            userId : id,
+            name :user.name,
+            email: user.email
+        }
+    }
+    var token = jwt.sign(payload,jwtPassphrase)
+    return token 
+}
+
+module.exports.verifyJWT = function(token){
+    var isValid = jwt.verify(token,jwtPassphrase)
+    console.log(isValid)
 }
